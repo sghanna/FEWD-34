e9d677dc50536a8e83912a834691a276eec26576
Shawn Hanna <sghanna@gmail.com>
2015-09-03 18:32:59 -0700

    week 7 start

---
+++ JSrecipes/JSfunctions.js
@@ -0,0 +1,25 @@
+<style>
+var el = document.createElement("li");
+el.textContent = "Go grocery shopping...";
+var parentEl = document.querySelector("ul");
+parentEl.appendChild(el);
+
+var name ="li";
+var text = "Go grocery shopping...";
+var parent = "ul";
+
+var el = document.createElement(name);
+el.textContent = text;
+var parentEl.document.querySelector(parent);
+parentEl.appendChild(el);
+
+function addElement(name, parent, text) {
+  var el = document.createElement(name);
+  el.textContent = text;
+  var parentEl.document.querySelector(parent);
+  parentEl.appendChild(el);
+}
+
+addElement("li", "ul", "Go grocery shoping";)
+
+</style>
